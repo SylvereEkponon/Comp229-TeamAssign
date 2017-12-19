dbf79c1168770bbcc95108202a7ce58bc9daa219
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-12-19 11:31:03 -0500

    Added Default and ShoppingCart pages

--- Comp229-TeamAssign/MusicStore/ShoppingCart.aspx.cs
+++ Comp229-TeamAssign/MusicStore/ShoppingCart.aspx.cs
@@ -5,19 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-//using statements that are required to connect to Entity Framework
-using Comp229_TeamAssign.Models;
-using System.Web.ModelBinding;
-
-namespace Comp229_TeamAssign
+namespace Comp229_TeamAssign.MusicStore
 {
-    public partial class Default : System.Web.UI.Page
+    public partial class ShoppingCart : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-
-
     }
 }
