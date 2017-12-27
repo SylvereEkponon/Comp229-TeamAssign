f46aadf392e57676f760994130251386e5cadae5
Anupom Roy <anupomr@yahoo.com>
2017-12-27 13:40:29 -0500

    Startup and Login page created

---
+++ Comp229-TeamAssign/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+//Required for OWIN Startup
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(Comp229_TeamAssign.Startup))]
+
+namespace Comp229_TeamAssign
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Login.aspx")
+            });
+
+        }
+    }
+}
