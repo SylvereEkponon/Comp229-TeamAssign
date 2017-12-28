9bf9a7f02e1fa5e6daf8d5a22fd6d184bed790b2
Anupom Roy <anupomr@yahoo.com>
2017-12-28 15:47:32 -0500

    Added Register and Logout functionality

--- Comp229-TeamAssign/Logout.aspx.cs
+++ Comp229-TeamAssign/Logout.aspx.cs
@@ -5,13 +5,22 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// required for Identity and OWIN Security
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace Comp229_TeamAssign
 {
     public partial class Logout : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+           
+            authenticationManager.SignOut();
+            
+            Response.Redirect("~/Login.aspx");
         }
     }
 }
--- Comp229-TeamAssign/Register.aspx.cs
+++ Comp229-TeamAssign/Register.aspx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
+
 namespace Comp229_TeamAssign
 {
     public partial class Register : System.Web.UI.Page
@@ -16,11 +21,45 @@ namespace Comp229_TeamAssign
 
         protected void CancelButton_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("~/Default.aspx");
         }
 
         protected void RegisterButton_Click(object sender, EventArgs e)
         {
+            //Create new userStore and userManeger objects;
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            //Creat a new user object 
+            var user = new IdentityUser()
+            {
+                UserName = UserNameTextBox.Text,
+                PhoneNumber = PhoneNumberTextBox.Text,
+                Email = EmailTextBox.Text
+            };
+
+            // create a new user in the database and store the results
+            IdentityResult result = userManager.Create(user, PasswordTextBox.Text);
+
+            // check if successfully registered?
+            if (result.Succeeded)
+            {
+                //authenticate and login our new user
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                //Sign in user
+                authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+
+                // Redirect  to the main Maenu Page
+                Response.Redirect("~/MusicStore/AddAlbum.aspx");
+            }
+            else
+            {
+                //display error in AlertFlash div
+                StatusLabel.Text = result.Errors.FirstOrDefault();
+                AlertFlash.Visible = true;
+            }
 
         }
     }
